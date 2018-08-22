9703d1244d2790699e03d2434fa1c3ff7d98f540
Pooja patel <poojavpatel71@gmail.com>
2018-08-22 23:42:29 +0530

    wrote another code for connection|created a schema | added gitignore

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,25 @@
+ /*jshint esversion: 6 */
 const mongoose = require('mongoose');
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo');
+// mongoose.connect('mongodb://localhost/playground');
+// //once connection is open fire this function
+// mongoose.connection.once('open',function(){
+// 	console.log('Wohooo the connection has been made');
+// }).on('error',function(error){
+// 	console.log('connection error :' , error );
+// });
 
-//once connection is open fire this function
-mongoose.connection.once('open',function(){
-	console.log('Wohooo the connection has been made');
-}).on('error',function(error){
-	console.log('connection error :' , error );
+//OR you can do
+mongoose.connect('mongodb://localhost/playground')
+	.then(() => console.log('connected to MongoDB....'))
+	.catch(err => console.log('could not connect to MongoDB....',err));
+// since .connect returns a promise we can use then and catch
+
+const CourseSchema = mongoose.Schema({
+	name:String,
+	author:String,
+	tags:[String],
+	date:{ type:Date , default:Date.now},
+	isPublished:Boolean
 });
