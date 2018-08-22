5daf8d71189d3101d1ee18fdc18ef296385643be
Pooja patel <poojavpatel71@gmail.com>
2018-08-23 00:27:12 +0530

    made 3 courses and added to MongoDB database|verifed in mongodb compass

--- test/connection.js
+++ test/connection.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost/playground')
 	.catch(err => console.log('could not connect to MongoDB....',err));
 // since .connect returns a promise we can use then and catch
 
+// creating schema
 const CourseSchema = mongoose.Schema({
 	name:String,
 	author:String,
@@ -23,3 +24,26 @@ const CourseSchema = mongoose.Schema({
 	date:{ type:Date , default:Date.now},
 	isPublished:Boolean
 });
+
+// compiling schema into a model
+// .model('singular name of collection',schema that defines shape of documents in this collecion)
+// this Course is a class
+const Course = mongoose.model( 'Course' , CourseSchema);
+
+async function createCourse(){
+	// this course is object of class Course
+	const course = new Course({
+		name:"React course",
+		author:"Pooja",
+		tags:['react','frontend'],
+		isPublished:true
+	});
+
+	//saving this object as document in database
+	// .save() is an async method , it returns a promise ,hence we can await it
+	// with await we need a async function hence we wrap course obect and saving in 
+	// an async function n then call it
+	const result = await course.save();
+	console.log(result);
+}
+createCourse();
